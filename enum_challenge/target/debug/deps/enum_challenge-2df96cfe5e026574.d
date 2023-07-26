1e54e310c2322fb5d636d069193fdcd738cf6079
endiesworld <endiesworld@gmail.com>
2023-07-26 02:34:21 +0100

    feat: Added challenge on enums

---
+++ enum_challenge/target/debug/deps/enum_challenge-2df96cfe5e026574.d
@@ -0,0 +1,5 @@
+/home/endie/Projects/Rust/Tutorials/common_concepts/enum_challenge/target/debug/deps/enum_challenge-2df96cfe5e026574: src/main.rs
+
+/home/endie/Projects/Rust/Tutorials/common_concepts/enum_challenge/target/debug/deps/enum_challenge-2df96cfe5e026574.d: src/main.rs
+
+src/main.rs:
---
+++ enum_challenge/target/debug/enum_challenge.d
@@ -0,0 +1,1 @@
+/home/endie/Projects/Rust/Tutorials/common_concepts/enum_challenge/target/debug/enum_challenge: /home/endie/Projects/Rust/Tutorials/common_concepts/enum_challenge/src/main.rs
